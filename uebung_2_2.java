14674f630aaec44ddeb9edc6c9d020bf20e0a8ee
Philovic <Philipp.Rehder@nordakademie.de>
2016-10-21 11:38:31 +0200

    Add files via upload

---
+++ uebung_2_2.java
@@ -0,0 +1,18 @@
+package uebung2_2;
+
+public class uebung_2_2 {
+
+	public static void main(String[] args) {
+		double a = 8;
+		double b = 3;
+		
+		double loesung1 = a * a + a * b + b * a + b * b;
+		double loesung2 = a *a + 2 * a * b + b * b;
+		
+		
+		
+		System.out.println("Lösung 1:" + loesung1);
+		System.out.println("Lösung 2:" + loesung2);
+	}
+
+}	
